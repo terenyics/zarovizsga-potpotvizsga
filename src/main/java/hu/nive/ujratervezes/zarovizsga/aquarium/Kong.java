e3eddf7254fb0fe357e88662c441975f3d4541dd
terenyicsaba <terenyicsaba@gmail.com>
2021-03-26 21:11:24 +0100

    aquarium commitja

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/aquarium/Kong.java
@@ -0,0 +1,14 @@
+package hu.nive.ujratervezes.zarovizsga.aquarium;
+
+public class Kong extends Fish {
+
+    public Kong(String name, int weight, String color) {
+        super(name, weight, color, false);
+    }
+
+    @Override
+    public void feed() {
+        super.feed();
+        this.weight += 2;
+    }
+}
